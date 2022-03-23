b3366083b64c09edeac35d9f36265d21e4f1cca8
himanshubjjain <100591203+himanshubjjain@users.noreply.github.com>
2022-03-23 20:56:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,23 @@ HTML_BANNER = """
     """
 
 def main():
-	st.title("Session States")
+	st.title("Lip Sync")
 
+st.subheader("How to Receive User Input")
+name = st.text_input("Enter Your Name","Type Here")
+result_name = name.title()
+st.write(result_name)
 
+## How to do Upload of Files
+# Solution By Adrien Treuile @streamlit
+#NB New Updates may include this feature request
+def file_selector(folder_path='.'):
+	    filenames = os.listdir(folder_path)
+	    selected_filename = st.selectbox('Select a file', filenames)
+	    return os.path.join(folder_path, selected_filename)
+
+	filename = file_selector()
+	st.write('You selected `%s`' % filename)
 	menu = ["Home","Custom_Settings","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
