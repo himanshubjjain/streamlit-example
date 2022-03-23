f092ec5b2db7e3477517119f2d7cd46c585ff779
himanshubjjain <100591203+himanshubjjain@users.noreply.github.com>
2022-03-23 20:56:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,10 @@ def file_selector(folder_path='.'):
 	    selected_filename = st.selectbox('Select a file', filenames)
 	    return os.path.join(folder_path, selected_filename)
 
-	filename = file_selector()
-	st.write('You selected `%s`' % filename)
-	menu = ["Home","Custom_Settings","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+filename = file_selector()
+st.write('You selected `%s`' % filename)
+menu = ["Home","Custom_Settings","About"]
+choice = st.sidebar.selectbox("Menu",menu)
 
 	# Home
 	if choice == "Home":
