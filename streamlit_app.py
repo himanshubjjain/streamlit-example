5646c286ee351b153238abec8d4171ab5567dbf2
himanshubjjain <100591203+himanshubjjain@users.noreply.github.com>
2022-03-23 21:20:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,9 @@ st.set_page_config(
      page_title='Streamlit cheat sheet',
      layout="wide",
      initial_sidebar_state="expanded",
+
+
+ name = st.text_input("Input the message","Type Here")
+ result_name = name.title()
+ st.write(result_name)
 )
-
-
-name = st.text_input("Input the message","Type Here")
-result_name = name.title()
-st.write(result_name)
-
