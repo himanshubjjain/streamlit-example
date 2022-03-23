02083239eac790b50d6861ef7ce3a52676039429
himanshubjjain <100591203+himanshubjjain@users.noreply.github.com>
2022-03-23 20:45:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ def file_selector(folder_path='.'):
 	    selected_filename = st.selectbox('Select a file', filenames)
 	    return os.path.join(folder_path, selected_filename)
 
-	filename = file_selector()
-	st.write('You selected `%s`' % filename)
+filename = file_selector()
+st.write('You selected `%s`' % filename)
