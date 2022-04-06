0ce0342b7cfb95d3766a925535c1672ba7bee46d
himanshubjjain <100591203+himanshubjjain@users.noreply.github.com>
2022-04-06 16:32:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as stc
 
 # File Processing Pkgs
-import pandas as pd
+
 from PIL import Image 
 
-
-
-# import fitz  # this is pymupdf
-
-# def read_pdf_with_fitz(file):
-# 	with fitz.open(file) as doc:
-# 		text = ""
-# 		for page in doc:
-# 			text += page.getText()
-# 		return text 
-
-# Fxn
 @st.cache
 def load_image(image_file):
 	img = Image.open(image_file)
@@ -43,19 +31,15 @@ def main():
 
 			img = load_image(image_file)
 			st.image(img)
-			
-  	 elif choice == "Audio":
+
+
+	elif choice == "Audio":
 		st.subheader("Audio")
 		audio_file = st.file_uploader("Upload Audio",type=['wav'])
 		if st.button("Process"):
 			if audio_file is not None:
-				file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+				file_details = {"Filename":audio_file.name,"FileType":audio_file.type,"FileSize":audio_file.size}
 				st.write(file_details)
 
-				audio_bytes=audio_file.read()
+				audio_bytes = audio_file.read(audio_file)
 				st.audio(audio_bytes,format='audio/wav')
-
-
-
-if __name__ == '__main__':
-	main()
